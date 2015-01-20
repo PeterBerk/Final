1631b40a606acf58e80e01958356e74bef72bdc2
peter <pete9160@scdsb.on.ca>
2015-01-20 09:00:04 -0500

    Patient class filled.

--- src/Patient.java
+++ src/Patient.java
@@ -2,5 +2,16 @@
  * Created by peter on 20/01/15.
  */
 public class Patient {
-
+    String name;
+    int priority;
+    public Patient(String name, int prio){
+        this.name = name;
+        this.priority = prio;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getPriority(){
+        return this.priority;
+    }
 }
