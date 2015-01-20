dfd9304fce45bbfb85e2539a6ecf9d33d7d03867
peter <pete9160@scdsb.on.ca>
2015-01-19 22:33:43 -0500

    Testing adding listeners to buttons

--- src/TaskApp.java
+++ src/TaskApp.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by peter on 19/01/15.
  */
 public class TaskApp {
+    private JButton button1;
+    private JPanel panel1;
+
+    public TaskApp() {
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("Button pushed!");
+            }
+        });
+    }
 }
